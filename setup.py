f655fb9f21317fd2c15afa8d3e1b2ecb7ca71706
Yanchao Liu <35838268+profyliu@users.noreply.github.com>
2023-01-12 22:09:40 -0500

    Revised MANIFEST CMakeLists README

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools.command.build_ext import build_ext
 
 
 setup(name='cubrif',
-        version="1.4.0",
-        description = "Build decision trees and random forests for classification and regression.",
+        version="1.4.1",
+        description = "Build random forests using CUDA GPU.",
         author = 'Yanchao Liu',
         author_email = 'yanchaoliu@wayne.edu',
         url = 'https://pypi.org/project/cubrif/',
